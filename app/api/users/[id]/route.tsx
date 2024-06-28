2e5b054453730267e65e49e5d3aecd7a77aa644c
Kyle Lerio <kylelerio15@gmail.com>
2024-06-28 13:27:02 +0800

    Deleting an object

--- app/api/users/[id]/route.tsx
+++ app/api/users/[id]/route.tsx
@@ -30,3 +30,16 @@ export async function PUT(
   // return the updated user
   return NextResponse.json({ id: 1, name: body.name });
 }
+
+export function DELETE(
+  request: NextRequest,
+  { params }: { params: { id: number } }
+) {
+  // Fetch use from db
+  // If not found, return 404
+  if (params.id > 10)
+    return NextResponse.json({ error: "User not found" }, { status: 404 });
+  // Delete the user
+  // Return 200
+  return NextResponse.json({});
+}
